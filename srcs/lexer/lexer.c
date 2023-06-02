e6f846dd2e969146a06bacd4ad851d9909f23c27
Hanmin Park <hanmpark@student.42nice.fr>
2023-06-02 09:03:23 +0200

    ✨📁 Created libs/ and starting the lexer

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -1,19 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/01 14:07:03 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/06/02 08:03:35 by hanmpark         ###   ########.fr       */
+/*   Created: 2023/06/02 08:03:50 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/06/02 08:06:20 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+/* Scans the input text character by character and groups characters
+* into meaningful units, such as keywords, identifiers, operators, and literals
+*/
+void	lexer()
 {
 
-	return (0);
 }
