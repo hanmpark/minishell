e54184f5808f60a2ec09b98db3edea4b4e5440ae
Hanmin Park <hanmin.01.park@gmail.com>
2023-07-30 23:19:08 +0200

    ✨⚙️ Renamed a variable for clarity

--- lib/libft/src/structft/ft_tokadd_back.c
+++ lib/libft/src/structft/ft_tokadd_back.c
@@ -3,13 +3,13 @@
 // Adds a new node at the end of a linked list.
 void	ft_tokadd_back(t_token **lst, t_token *new)
 {
-	t_token	*theone;
+	t_token	*node;
 
 	if (!*lst)
 		*lst = new;
 	else
 	{
-		theone = ft_toklast(*lst);
-		theone->next = new;
+		node = ft_toklast(*lst);
+		node->next = new;
 	}
 }
