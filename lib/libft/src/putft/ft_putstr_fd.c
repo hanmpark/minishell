a8eded685eb073800fc0bbc6f394676ba422e486
Hanmin Park <hanmin.01.park@gmail.com>
2023-07-31 20:15:05 +0200

    ✨🔨 Fixed synchronization for the error's output when piping

--- lib/libft/src/putft/ft_putstr_fd.c
+++ lib/libft/src/putft/ft_putstr_fd.c
@@ -1,13 +1,10 @@
 #include "putft.h"
+#include "charft.h"
 
 // Writes a string to a specified file descriptor.
 void	ft_putstr_fd(char *s, int fd)
 {
 	if (!s)
 		return ;
-	while (*s)
-	{
-		ft_putchar_fd(*s, fd);
-		s++;
-	}
+	write(fd, s, ft_strlen(s));
 }
